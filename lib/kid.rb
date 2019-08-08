69eecd9cfa37812a25b0979186f44a9eb0b977a4
jessi ortega <ortegabx05@gmail.com>
2019-08-08 14:22:17 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,9 +2,9 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  extend Metadancing
+  
   include Dance
- 
+ extend MetaDancing
  
   attr_accessor :name
  
